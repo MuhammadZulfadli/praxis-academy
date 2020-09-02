48bae5fc6ce11bf29c971ebf523d4e259688057e
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-09-02 23:15:49 +0700

    finished

---
+++ novice/03-02/kasus/kasus-cli/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div id="demo">
+      <form id="search">
+        Search
+        <input name="query" v-model="searchQuery" />
+      </form>
+      <grid :heroes="gridData" :columns="gridColumns" :filter-key="searchQuery"></grid>
+    </div>
+    <br />
+    <div id="app">
+      <button id="show-modal" @click="showModal = true">Show Modal</button>
+      <!-- use the modal component, pass in the prop -->
+      <modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+        -->
+        <h3 slot="header">custom header</h3>
+      </modal>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Grid from "./components/Grid";
+import Modal from "./components/Modal";
+
+export default {
+  name: "App",
+  components: {
+    Grid,
+    Modal,
+  },
+  data: function () {
+    return {
+      searchQuery: "",
+      gridColumns: ["name", "power"],
+      gridData: [
+        { name: "Chuck Norris", power: Infinity },
+        { name: "Bruce Lee", power: 9000 },
+        { name: "Jackie Chan", power: 7000 },
+        { name: "Jet Li", power: 8000 },
+        { name: "Ip Man", power: 100000 },
+      ],
+      showModal: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ novice/03-02/kasus/kasus-cli/src/components/Grid.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th
+            v-for="(key, data) in columns"
+            @click="sortBy(key)"
+            :class="{ active: sortKey == key }"
+            :key="data"
+          >
+            {{ key | capitalize }}
+            <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'"></span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(entry, data) in filteredHeroes" :key="data">
+          <td v-for="(key, data) in columns" :key="data">{{entry[key]}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+// register the grid component
+export default {
+  name: "Grid",
+  props: {
+    heroes: Array,
+    columns: Array,
+    filterKey: String,
+  },
+  data: function () {
+    var sortOrders = {};
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1;
+    });
+    return {
+      sortKey: "",
+      sortOrders: sortOrders,
+    };
+  },
+  computed: {
+    filteredHeroes: function () {
+      var sortKey = this.sortKey;
+      var filterKey = this.filterKey && this.filterKey.toLowerCase();
+      var order = this.sortOrders[sortKey] || 1;
+      var heroes = this.heroes;
+      if (filterKey) {
+        heroes = heroes.filter(function (row) {
+          return Object.keys(row).some(function (key) {
+            return String(row[key]).toLowerCase().indexOf(filterKey) > -1;
+          });
+        });
+      }
+      if (sortKey) {
+        heroes = heroes.slice().sort(function (a, b) {
+          a = a[sortKey];
+          b = b[sortKey];
+          return (a === b ? 0 : a > b ? 1 : -1) * order;
+        });
+      }
+      return heroes;
+    },
+  },
+  filters: {
+    capitalize: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key;
+      this.sortOrders[key] = this.sortOrders[key] * -1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+body {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+
+table {
+  border: 2px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+th {
+  background-color: #42b983;
+  color: rgba(255, 255, 255, 0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th,
+td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
+th.active {
+  color: #fff;
+}
+
+th.active .arrow {
+  opacity: 1;
+}
+
+.arrow {
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  height: 0;
+  margin-left: 5px;
+  opacity: 0.66;
+}
+
+.arrow.asc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-bottom: 4px solid #fff;
+}
+
+.arrow.dsc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid #fff;
+}
+</style>
